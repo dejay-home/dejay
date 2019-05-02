ea20f39f199b200029f10fdf0e346b98b52a9b39
mongsaeng <mongsaeng@dejay.co.kr>
2019-05-02 13:31:44 +0900

    update

--- js/business.js
+++ js/business.js
@@ -10,10 +10,10 @@ $(document).ready(function(){
 
 function sectionClick(){
 
-    var company_inner01 = $('.company_inner01').offset().top;
-    var company_inner02 = $('.company_inner02').offset().top;
-    var company_inner03 = $('.company_inner03').offset().top;
-    var company_inner04 = $('.company_inner04').offset().top;
+    var business_inner01 = $('.business_inner01').offset().top;
+    var business_inner02 = $('.business_inner02').offset().top;
+    var business_inner03 = $('.business_inner03').offset().top;
+    var business_inner04 = $('.business_inner04').offset().top;
 	
     $(".btn_top").click(function(){
 		$('html, body').animate({
@@ -23,25 +23,25 @@ function sectionClick(){
     
     $(".section_link01").click(function(){
 		$('html, body').animate({
-			scrollTop:company_inner04
+			scrollTop:business_inner04
 		},300, 'swing');
 	});
 	
 	$(".section_link02").click(function(){
 		$('html, body').animate({
-			scrollTop:company_inner03
+			scrollTop:business_inner03
         },300, 'swing');
 	});
 	
 	$(".section_link03").click(function(){
 		$('html, body').animate({
-			scrollTop:company_inner02
+			scrollTop:business_inner02
 		},300, 'swing');
     });
     
     $(".section_link04").click(function(){
 		$('html, body').animate({
-			scrollTop:company_inner01
+			scrollTop:business_inner01
         },300, 'swing');
     });
     
@@ -49,44 +49,42 @@ function sectionClick(){
 
 function scrollAction (){
 
-    var company_inner01_H = $('.company_inner01').offset().top;
-    var company_inner02_H = $('.company_inner02').offset().top;
-    var company_inner03_H = $('.company_inner03').offset().top;
-    var company_inner04_H = $('.company_inner04').offset().top;
+    var business_inner01_H = $('.business_inner01').offset().top;
+    var business_inner02_H = $('.business_inner02').offset().top;
+    var business_inner03_H = $('.business_inner03').offset().top;
+    var business_inner04_H = $('.business_inner04').offset().top;
 
     $(window).on('scroll', function() {
 
         var actionScroll = $(this).scrollTop() + 400;
         
-        if (company_inner01_H < actionScroll) {
-            $('.company_inner01').addClass('active');
+        if (business_inner01_H < actionScroll) {
+            $('.business_inner01').addClass('active');
             setTimeout(function() { 
-                $('.company_inner01 .info').addClass('active'); 
+                $('.business_inner01 .info').addClass('active'); 
             }, 300);
-        };
+        }
 
-        if (company_inner02_H < actionScroll) {
-            $('.company_inner02').addClass('active');
+        if (business_inner02_H < actionScroll) {
+            $('.business_inner02').addClass('active');
             setTimeout(function() { 
-                $('.company_inner02 .info').addClass('active'); 
+                $('.business_inner02 .info').addClass('active'); 
             }, 300);
-        };
+        }
 
-        if (company_inner03_H < actionScroll) {
-            $('.company_inner03').addClass('active');
+        if (business_inner03_H < actionScroll) {
+            $('.business_inner03').addClass('active');
             setTimeout(function() { 
-                $('.company_inner03 .info').addClass('active'); 
+                $('.business_inner03 .info').addClass('active'); 
             }, 300);
-        };
+        }
 
-        if (company_inner04_H < actionScroll) {
-            $('.company_inner04').addClass('active');
+        if (business_inner04_H < actionScroll) {
+            $('.business_inner04').addClass('active');
             setTimeout(function() { 
-                $('.company_inner04 .info').addClass('active'); 
+                $('.business_inner04 .info').addClass('active'); 
             }, 300);
-        };
+        }
     });
-	//alert(visual_area_H);
-
     
 };
