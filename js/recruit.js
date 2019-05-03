ba17a495c3de219c482773cf2cf8f2ff9d7d00a4
mongsaeng <mongsaeng@dejay.co.kr>
2019-05-03 17:43:05 +0900

    작업마무리0503

--- js/recruit.js
+++ js/recruit.js
@@ -1,3 +1,100 @@
 $(document).ready(function(){  
-  
+    var visual_area = $(window).height();
+    $(".visual_area").css('height', visual_area);
+
+    $('.content').tabs({
+        active:0,
+        activate: function( event, ui ){
+            var selectedTab = $(".content").tabs('option', 'active');
+            if(selectedTab == 0){
+                $('.rnb').fadeIn();
+            }
+            else if(selectedTab == 1){
+                $('.rnb').fadeOut();
+            }
+        }
+    });
+    sectionClick();
+    scrollAction();
 });
+
+function sectionClick(){
+
+    var recruit_inner01 = $('.recruit_inner01').offset().top;
+    var recruit_inner02 = $('.recruit_inner02').offset().top;
+    var recruit_inner03 = $('.recruit_inner03').offset().top;
+    var recruit_inner04 = $('.recruit_inner04').offset().top;
+	
+    $(".btn_top").click(function(){
+		$('html, body').animate({
+			scrollTop:0
+		},300, 'swing');
+    });
+    
+    $(".section_link01").click(function(){
+		$('html, body').animate({
+			scrollTop:recruit_inner04
+		},300, 'swing');
+	});
+	
+	$(".section_link02").click(function(){
+		$('html, body').animate({
+			scrollTop:recruit_inner03
+        },300, 'swing');
+	});
+	
+	$(".section_link03").click(function(){
+		$('html, body').animate({
+			scrollTop:recruit_inner02
+		},300, 'swing');
+    });
+    
+    $(".section_link04").click(function(){
+		$('html, body').animate({
+			scrollTop:recruit_inner01
+        },300, 'swing');
+    });
+    
+};
+
+function scrollAction (){
+
+    var recruit_inner01_H = $('.recruit_inner01').offset().top;
+    var recruit_inner02_H = $('.recruit_inner02').offset().top;
+    var recruit_inner03_H = $('.recruit_inner03').offset().top;
+    var recruit_inner04_H = $('.recruit_inner04').offset().top;
+
+    $(window).on('scroll', function() {
+
+        var actionScroll = $(this).scrollTop() + 200;
+        
+        if (recruit_inner01_H < actionScroll) {
+            $('.recruit_inner01').addClass('active');
+            setTimeout(function() { 
+                $('.recruit_inner01 .info').addClass('active'); 
+            }, 300);
+        }
+
+        if (recruit_inner02_H < actionScroll) {
+            $('.recruit_inner02').addClass('active');
+            setTimeout(function() { 
+                $('.recruit_inner02 .info').addClass('active'); 
+            }, 300);
+        }
+
+        if (recruit_inner03_H < actionScroll) {
+            $('.recruit_inner03').addClass('active');
+            setTimeout(function() { 
+                $('.recruit_inner03 .info').addClass('active'); 
+            }, 300);
+        }
+
+        if (recruit_inner04_H < actionScroll) {
+            $('.recruit_inner04').addClass('active');
+            setTimeout(function() { 
+                $('.recruit_inner04 .info').addClass('active'); 
+            }, 300);
+        }
+    });
+    
+};
--- js/reference.js
+++ js/reference.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){  
+    
+    $(".btn_top").click(function(){
+		$('html, body').animate({
+			scrollTop:0
+		},300, 'swing');
+    });
     var $grid = $('.reference_area').isotope({
         itemSelector:'.grid-item',
         layoutMode:'fitRows',
@@ -12,27 +18,27 @@ $(document).ready(function(){
     });
 
     // 레퍼런스 상세화면
-    $('.reference_detail').show();
-    // $('.btn_reference').click(function(){
-    //     var $href = $(this).attr('href');
-    //     layer_popup($href);
-    //     //$('.detail_area, .reference_detail').fadeIn();
-    // });
-    // function layer_popup(el){
+    $('.reference_detail').hide();
+    $('.btn_reference').click(function(){
+        var $href = $(this).attr('href');
+        layer_popup($href);
+        //$('.detail_area, .reference_detail').fadeIn();
+    });
+    function layer_popup(el){
 
-    //     var $el = $(el);
-    //     var isDim = $el.prev().hasClass('dim');
+        var $el = $(el);
+        var isDim = $el.prev().hasClass('dim');
 
-    //     isDim ? $('.reference_detail').fadeIn().addClass('on') : $el.fadeIn();
-    //     $el.find('a.btn-layerClose').click(function(){
-    //         isDim ? $('.reference_detail').fadeOut() : $el.fadeOut();
-    //         return false;
-    //     });
+        isDim ? $('.reference_detail').fadeIn().addClass('on') : $el.fadeIn();
+        $el.find('a.btn-layerClose').click(function(){
+            isDim ? $('.reference_detail').fadeOut() : $el.fadeOut();
+            return false;
+        });
 
-    //     // $('.layer .dimBg').click(function(){
-    //     //     $('.dim-layer').fadeOut();
-    //     //     return false;
-    //     // });
+        // $('.layer .dimBg').click(function(){
+        //     $('.dim-layer').fadeOut();
+        //     return false;
+        // });
 
-    // }
+    }
 });
